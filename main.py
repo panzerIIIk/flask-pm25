c0a67783c4f8d7a982d358063c613be6815912ab
tim <a0927555643@gmail.com>
2026-05-10 16:07:15 +0800

    未完成最新數據取得

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import database
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+
+    result = database.get_latest_data()
+    return render_template("index.html", result=result)
+
 
 if __name__ == "main":
     app.run(debug=True)
